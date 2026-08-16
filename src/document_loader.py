6aae3d17c2f6143ac6b2d0bf2ec5b2c5ddc9f123
ammar869 <ammarhassan1131318@gmail.com>
2026-08-17 03:59:01 +0500

    loading

--- src/document_loader.py
+++ src/document_loader.py
@@ -3,7 +3,29 @@ import pymupdf
 file_path = "C:/Users/Ammar/Documents/DocuMind_AI/data/documents/sample_professional_invoice.pdf"
 def load_pdf(file_path):
     document = fitz.open(file_path)
-    print(len(document))
+    #print(len(document))
     page = document[0]
+    #print(page.get_text())
+
+    #for page in document:
+    #    print(page.get_text())
+    #   pass
+
+    for page_number, page in enumerate(document, start=1):
+        print("PAGE:", page_number)
+        print(page.get_text())
+
+    pages = []
+
+    for page_number, page in enumerate(document, start=1):
+
+        text = page.get_text()
+
+        page_data = {
+        # page number
+        # text
+    }
+
+    pages.append(page_data)
 
 load_pdf(file_path)
