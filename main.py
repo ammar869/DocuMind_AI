daa07a14bfffb60d6338d4ec208aa78337d43680
ammar869 <ammarhassan1131318@gmail.com>
2026-08-18 01:20:09 +0500

    Indexing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.document_loader import load_pdf
 from src.Model import llm
 from src.processor import clean_pages
 from src.structurer import structure_document
+from src.indexing.chunker import chunk_pages
 file_path = "C:/Users/Ammar/Documents/DocuMind_AI/data/documents/sample_professional_invoice.pdf"
 
 pages = load_pdf(file_path)
